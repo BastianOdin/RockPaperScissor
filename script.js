570e38bdb0b97f7d0041d6bd8cccf632ddcd4258
BastianOdin <121452440+BastianOdin@users.noreply.github.com>
2023-02-08 09:48:23 +0100

    adds first part of pseudo code

---
+++ script.js
@@ -0,0 +1,13 @@
+/* 
+We need a function called "getComputerChoice" that randomly returns "Rock, Paper or Scissor"
+
+We need a function that Plays one round of RPS
+Take Computer Choice and Player Choice and return something like:
+"You loose! Paper beats Rock!"
+parameter players choice needs to be case insensitive
+Return result do not put it in console. We need it in the next function called Game
+
+
+
+
+*/
